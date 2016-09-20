584a98baef291f594154659005272b1912985e01
Klaus Kazlauskas <klausmachado@gmail.com>
2016-09-20 00:24:44 -0300

    Musician class comments

--- app/src/main/java/br/com/klauskpm/musicapp/Musician.java
+++ app/src/main/java/br/com/klauskpm/musicapp/Musician.java
@@ -15,31 +15,59 @@ public class Musician {
 
     public Musician() {}
 
+    /**
+     * Musician class
+     *
+     * @param mName of the musician
+     * @param img for the musician thumbnail
+     */
     public Musician(String mName, Drawable img) {
         this.mName = mName;
         this.mImg = img;
     }
 
+    /**
+     * @return the musician name
+     */
     public String getmName() {
         return mName;
     }
 
+    /**
+     * Set the musician name
+     * @param mName of the musician
+     */
     public void setmName(String mName) {
         this.mName = mName;
     }
 
+    /**
+     * @return musician image/thumbnail
+     */
     public Drawable getmImg() {
         return mImg;
     }
 
+    /**
+     * Set the musician image/thumbnail
+     * @param mImg for the musician thumbnail
+     */
     public void setmImg(Drawable mImg) {
         this.mImg = mImg;
     }
 
+    /**
+     * @return a musician's albums list
+     */
     public List<Album> getmArrayAlbums() {
         return mArrayAlbums;
     }
 
+    /**
+     *
+     * @param position of desired album of the list
+     * @return an album from the album's list
+     */
     public Album getmAlbum(int position) {
         return mArrayAlbums.get(position);
     }
@@ -52,6 +80,10 @@ public class Musician {
         return album;
     }
 
+    /**
+     * Set an list of albums
+     * @param mArrayAlbums from the musician
+     */
     public void setmArrayAlbums(List<Album> mArrayAlbums) {
         this.mArrayAlbums = mArrayAlbums;
     }
